3d4142c78d3d6bc7d29dec46dfb74d6e3033ed97
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-09-13 23:34:33 -0700

    created and tested Tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,6 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll({include: [Product]}).then(tagData => res.json(tagData)).catch(err => res.json(err))
   // find all tags
   // be sure to include its associated Product data
 });
@@ -11,18 +12,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({where: {id: req.params.id}, include: [Product]}).then(tagData => res.json(tagData)).catch(err => res.json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body).then(tagData => res.json(tagData)).catch(err => res.json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {where: {id: req.params.id}}).then(tagData => res.json(tagData)).catch(err => res.json(err))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({where: {id: req.params.id}})
 });
 
 module.exports = router;
